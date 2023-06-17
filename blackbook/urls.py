66ec4855fb48a8946291150d18dfe5d87255301f
Tomgxz <75934344+Tomgxz@users.noreply.github.com>
2023-06-17 13:55:15 +0000

    Static deployment

--- blackbook/urls.py
+++ blackbook/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.views.static import serve 
 
 from . import views
 
--- main/settings.py
+++ main/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-=q5e15&3$j=10djmou2=3s!(hmz6(wqpx#@mq!w0^%)=+#6u#+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.localhost', '127.0.0.1', '[::1]', "192.168.1.9", "192.168.0.10", "90e2e08a-3155-43d6-9745-6e6675a85759.id.repl.co", "blackbook-marketplace.tomgxz.repl.co"]
 
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -117,8 +118,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = "static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
