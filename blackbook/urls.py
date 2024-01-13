30f02ecd512375055676dc0387cbedc7b1f6d380
Tomgxz <thomaslee10.TL@gmail.com>
2024-01-13 14:51:37 +0000

    robots.txt

--- blackbook/urls.py
+++ blackbook/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from django.views.static import serve 
+from django.views.generic.base import TemplateView
 
 from . import views
 
@@ -10,4 +11,9 @@ urlpatterns = [
     path("marketplace/", views.marketplace, name="marketplace"),
     path("marketplace/item/<item_id>/<item_name>", views.marketplaceItem, name="marketplaceItem"),
     path("data/completely-safe-url", views.tracking, name="completelysafeurl"),
+    
+    path(
+        "robots.txt",
+        TemplateView.as_view(template_name="robots.txt", content_type="text/plain"),
+    )
 ]
